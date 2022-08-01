665a7646e71d8d0e9721c93f9ff519292c1085ec
mdbryan <meaghan.bryan77@gmail.com>
2022-08-01 13:21:56 -0700

    slope data and results

--- ebs.R
+++ ebs.R
@@ -6,7 +6,7 @@ devtools::install_github("BenWilliams-NOAA/swo",force=TRUE)
 #load packages
 library(swo)
 
-yrs = 2017
+yrs = 2002
 species = c(10110, 10112, 10115, 10130, 10210, 10261, 10285, 21720, 21740, 30060)
 region = 'BS'
 afsc_user = 'BRYANM'
@@ -20,7 +20,7 @@ strata_data <- vroom::vroom(here::here('data', 'strata_bs.csv'))
 specimen_data <- vroom::vroom(here::here('data', 'specimen_bs.csv'))
 
 iters = 500
-
+#ebs shelf
 cpue_data %>% 
   tidytable::filter.(!(species_code %in% c(30060))) -> .cpue
 lfreq_data %>% 
@@ -37,3 +37,24 @@ swo_sim(iters, .lfreq, .specimen, .cpue, strata_data, yrs,
 
 end <- Sys.time()
 
+#ebs slope
+cpue_data_s <- vroom::vroom(here::here('data', 'cpue_slopebs.csv'))
+lfreq_data_s <- vroom::vroom(here::here('data', 'lfreq_slope_bs.csv'))
+strata_data <- vroom::vroom(here::here('data', 'strata_bs.csv'))
+specimen_data_s <- vroom::vroom(here::here('data', 'specimen_slopebs.csv'))
+
+cpue_data_s %>% 
+  tidytable::filter.(species_code %in% c(10112, 10115,30060)) -> .cpue
+lfreq_data_s %>% 
+  tidytable::filter.(species_code %in% c(10112, 10115,30060)) -> .lfreq
+specimen_data_s %>% 
+  tidytable::filter.(species_code %in% c(10112, 10115,30060)) -> .specimen
+
+st <- Sys.time()
+
+swo_sim(iters, .lfreq, .specimen, .cpue, strata_data, yrs, 
+        strata = FALSE, boot_hauls = TRUE, boot_lengths = TRUE, 
+        boot_ages = TRUE, reduce_lengths = NULL, length_samples = NULL, sex_samples = NULL, save = 'input_ss', 
+        write_comp = FALSE, write_sample = FALSE, region = 'bs', save_orig = TRUE)
+
+end <- Sys.time()
